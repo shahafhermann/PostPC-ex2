41a2679e77caf2cc02f522c14700e1c14d07f7d6
shahaf.hermann <shahaf.hermann@cs.huji.ac.il>
2020-05-02 17:02:25 +0300

    Kind of working, had some strange bugs with adding new tasks

--- app/src/main/java/com/ppc/todoboom/MainActivity.kt
+++ app/src/main/java/com/ppc/todoboom/MainActivity.kt
@@ -1,30 +1,75 @@
 package com.ppc.todoboom
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+//    val create = findViewById<Button>(R.id.createButton)
+//    val inputText = findViewById<EditText>(R.id.inputText)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv = findViewById<TextView>(R.id.textView)
+//        val tv = findViewById<TextView>(R.id.textView)
+//
+//        create.setOnClickListener {
+//            if (inputText.text.equals("")) {
+//                Toast.makeText(applicationContext, R.string.empty_error, Toast.LENGTH_SHORT).show()
+//            } else {
+//                tv.text = inputText.text.toString()
+//                TODO("Add the new task to the recycler")
+//            }
+//            inputText.setText("")
+//        }
+
+        setupListAdapter()
+    }
+
+    private fun setupListAdapter(){
         val create = findViewById<Button>(R.id.createButton)
         val inputText = findViewById<EditText>(R.id.inputText)
 
+        val context = this
+        val tasks = mutableListOf<Task>()
+        val adapter = TaskListAdapter()
+
+        task_recycler.adapter = adapter
+        task_recycler.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+
+        adapter.onItemClickCallback = { task, holder ->
+            Toast.makeText(this, "Tap on ${task.description}", Toast.LENGTH_SHORT).show()
+            if (!task.isDone) {
+                task.isDone = true
+                holder.text.setTextColor(Color.argb(40, 180, 180, 180))
+                adapter.submitList(tasks.toList())
+            }
+        }
+
         create.setOnClickListener {
-            if (inputText.text.equals("")) {
+            if (inputText.text.toString() == "") {
                 Toast.makeText(applicationContext, R.string.empty_error, Toast.LENGTH_SHORT).show()
             } else {
-                tv.text = inputText.text.toString()
+                val task = Task(description = inputText.text.toString())
+
+//                if (tasks.isEmpty()) {
+//                    tasks += task
+//                } else {
+//                    val index = Random.nextInt(until = tasks.size)
+//                    tasks.add(index, task)
+//                }
+                tasks.add(task)
+                adapter.submitList(tasks)
+
             }
-            inputText.setText("")
         }
     }
 }
--- app/src/main/java/com/ppc/todoboom/TaskUiUtils.kt
+++ app/src/main/java/com/ppc/todoboom/TaskUiUtils.kt
@@ -1,2 +1,52 @@
 package com.ppc.todoboom
 
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+data class Task(val description: String) {
+    var isDone: Boolean = false
+}
+
+
+class TaskHolder(view: View): RecyclerView.ViewHolder(view) {
+    val text: TextView = view.findViewById(R.id.taskText)
+}
+
+private object TaskDiffCallback: DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem.description == newItem.description
+    }
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class TaskListAdapter: ListAdapter<Task, TaskHolder>(TaskDiffCallback) {
+    var onItemClickCallback: ((Task, TaskHolder) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
+        val context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_one_task, parent, false)
+        val holder = TaskHolder(view)
+
+        view.setOnClickListener {
+            val callback = onItemClickCallback ?: return@setOnClickListener
+            val task = getItem(holder.adapterPosition)
+            callback(task, holder)
+        }
+
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: TaskHolder, position: Int) {
+        val task = getItem(position)
+        holder.text.text = task.description
+    }
+}
